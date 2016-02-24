6638294191e8c8b5234b7f78a9fb544673bd32e1
Gary <frontrear>
2016-02-24 15:58:31 +0200

    Initial commit

---
+++ twitter-widgets.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name           RES Twitter Expando
+// @description    Loads the Twitter Widgets javascript file for use with RES in Firefox
+// @include        *reddit.com/*
+// @author         Sarkos
+// @require        https://platform.twitter.com/widgets.js
+// @grant          none
+// ==/UserScript==
+
+var siteTable = document.getElementById('siteTable');
+if (siteTable) {
+	new MutationObserver(function() {
+		twttr.widgets.load();
+	}).observe(siteTable, { childList:true, subtree:true });
+}
